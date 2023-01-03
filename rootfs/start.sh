cf40cb071e17e474c1335fd4b2cf18c1759a35d2
Roleplay Reality <development@roleplayreality.nl>
2023-01-03 21:25:09 +0100

    wine edits

--- rootfs/start.sh
+++ rootfs/start.sh
@@ -1,6 +1,9 @@
 #!/bin/bash
 rm -rf /home/container/.nginx/tmp/*
 
+export WINEDEBUG=-all,fixme-all
+export WINEARCH=win64
+
 echo "⟳ Starting Nginx..."
 /usr/sbin/nginx -c /home/container/.nginx/nginx/nginx.conf -p /home/container/.nginx/
 clear
